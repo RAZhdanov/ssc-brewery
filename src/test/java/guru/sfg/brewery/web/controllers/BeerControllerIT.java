39ee5007501374e4704924ae46fab462cadb19e3
ramzes.zhdanov <ramzes.zhdanov@mail.ru>
2022-01-03 18:59:44 +0300

    Assignment 2: Add User

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -27,8 +27,16 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 public class BeerControllerIT extends BaseIT {
 
     @Test
-    void initCreationForm() throws Exception {
+    void initCreationForm1() throws Exception {
         mockMvc.perform(get("/beers/new").with(httpBasic("user", "password")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+    @Test
+    void initCreationForm2() throws Exception {
+        mockMvc.perform(get("/beers/new").with(httpBasic("scott", "tiger")))
                 .andExpect(status().isOk())
                 .andExpect(view().name("beers/createBeer"))
                 .andExpect(model().attributeExists("beer"));
